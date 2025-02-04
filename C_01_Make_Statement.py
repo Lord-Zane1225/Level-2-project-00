f11bd3d9f59d28594a2ea0ca797fd75c5eaeb497
Lord_Zane021 <140982529+Lord-Zane1225@users.noreply.github.com>
2025-02-04 13:26:09 +1300

    Add files via upload
    Function to create statement

---
+++ C_01_Make_Statement.py
@@ -0,0 +1,11 @@
+# functions
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decorations
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+# main routine
+
+
+make_statement("Programming is fun!", "👍")

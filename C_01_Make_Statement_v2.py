77390f4a34847b2ecb201a6117c05eb7e580ca17
Lord_Zane021 <140982529+Lord-Zane1225@users.noreply.github.com>
2025-02-05 14:31:08 +1300

    Flexible Statement Generator and sandbox

---
+++ C_01_Make_Statement_v2.py
@@ -0,0 +1,27 @@
+# functions
+def make_statement(statement, decoration, lines):
+    """Emphasises headings by adding decorations
+    at the start and end"""
+
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+# main routine
+
+
+make_statement("Programming is fun!", "=", 3)
+print()
+make_statement("Programming is still fun!", "*", 2)
+print()
+make_statement("Emoji in action", "🐍", 1)
+print()
---
+++ Sandbox.py
@@ -0,0 +1,14 @@
+# reminder - an integer that has been multiplied by three
+number = 5
+print(number * 3)
+
+# string multiplied by 3
+# note speech marks and colour
+num_string = "5"
+print(num_string * 3)
+
+# an example showing what the
+example_text = "hello world"
+text_length = len(example_text)
+
+print(f"'{example_text}' is {text_length} characters long")
